b49420c4bb75a372fae0f171c2f6062e85e5efdf
Shin Thant Aung <118516554+shin-aung@users.noreply.github.com>
2025-11-20 22:02:54 +0800

    replacement for env

--- db.go
+++ db.go
@@ -11,15 +11,23 @@ import (
 
 var pool *pgxpool.Pool
 
+func getenv(key, defaultValue string) string {
+    value := os.Getenv(key)
+    if value == "" {
+        return defaultValue
+    }
+    return value
+}
+
 func initDB() error {
 	    dsn := fmt.Sprintf(
         "postgres://%s:%s@%s:%s/%s",
-		os.Getenv("DATABASE_USER"),
-		os.Getenv("DATABASE_PASSWORD"),
-		os.Getenv("DATABASE_HOST"),
-		os.Getenv("DATABASE_PORT"),
-		os.Getenv("DATABASE_NAME"),
-	)
+        getenv("DATABASE_USER", "pyou_developer"),
+        getenv("DATABASE_PASSWORD", "stockpassword123"),
+        getenv("DATABASE_HOST", "localhost"),
+        getenv("DATABASE_PORT", "5432"),
+        getenv("DATABASE_NAME", "stock_dashboard"),
+    )
 
     var err error
     ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
